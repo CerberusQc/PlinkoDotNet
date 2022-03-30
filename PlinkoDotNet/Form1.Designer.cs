3ecf1908eb3f026703980d9bfc9bbbc107507d3e
Bastien Roy-M <bastien@edgegap.com>
2022-03-30 13:43:12 -0400

    base project

---
+++ PlinkoDotNet/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace PlinkoDotNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GamePanel = new System.Windows.Forms.Panel();
+            this.poker5 = new System.Windows.Forms.PictureBox();
+            this.poker4 = new System.Windows.Forms.PictureBox();
+            this.poker3 = new System.Windows.Forms.PictureBox();
+            this.poker2 = new System.Windows.Forms.PictureBox();
+            this.poker1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.InputForm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPoker = new System.Windows.Forms.Button();
+            this.GamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.poker5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GamePanel
+            // 
+            this.GamePanel.BackColor = System.Drawing.Color.Lime;
+            this.GamePanel.Controls.Add(this.poker5);
+            this.GamePanel.Controls.Add(this.poker4);
+            this.GamePanel.Controls.Add(this.poker3);
+            this.GamePanel.Controls.Add(this.poker2);
+            this.GamePanel.Controls.Add(this.poker1);
+            this.GamePanel.Location = new System.Drawing.Point(12, 12);
+            this.GamePanel.Name = "GamePanel";
+            this.GamePanel.Size = new System.Drawing.Size(539, 506);
+            this.GamePanel.TabIndex = 0;
+            // 
+            // poker5
+            // 
+            this.poker5.Location = new System.Drawing.Point(274, 292);
+            this.poker5.Name = "poker5";
+            this.poker5.Size = new System.Drawing.Size(130, 180);
+            this.poker5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poker5.TabIndex = 4;
+            this.poker5.TabStop = false;
+            // 
+            // poker4
+            // 
+            this.poker4.Location = new System.Drawing.Point(84, 292);
+            this.poker4.Name = "poker4";
+            this.poker4.Size = new System.Drawing.Size(130, 180);
+            this.poker4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poker4.TabIndex = 3;
+            this.poker4.TabStop = false;
+            // 
+            // poker3
+            // 
+            this.poker3.Location = new System.Drawing.Point(333, 64);
+            this.poker3.Name = "poker3";
+            this.poker3.Size = new System.Drawing.Size(130, 180);
+            this.poker3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poker3.TabIndex = 2;
+            this.poker3.TabStop = false;
+            // 
+            // poker2
+            // 
+            this.poker2.Location = new System.Drawing.Point(178, 64);
+            this.poker2.Name = "poker2";
+            this.poker2.Size = new System.Drawing.Size(130, 180);
+            this.poker2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poker2.TabIndex = 1;
+            this.poker2.TabStop = false;
+            // 
+            // poker1
+            // 
+            this.poker1.Location = new System.Drawing.Point(27, 64);
+            this.poker1.Name = "poker1";
+            this.poker1.Size = new System.Drawing.Size(130, 180);
+            this.poker1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poker1.TabIndex = 0;
+            this.poker1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Plinko!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputForm
+            // 
+            this.InputForm.Location = new System.Drawing.Point(638, 52);
+            this.InputForm.Name = "InputForm";
+            this.InputForm.Size = new System.Drawing.Size(100, 20);
+            this.InputForm.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(676, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mise";
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.AcceptsReturn = true;
+            this.MessageBox.AcceptsTab = true;
+            this.MessageBox.Location = new System.Drawing.Point(596, 175);
+            this.MessageBox.Multiline = true;
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.ReadOnly = true;
+            this.MessageBox.Size = new System.Drawing.Size(180, 343);
+            this.MessageBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(659, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Messages";
+            // 
+            // btnPoker
+            // 
+            this.btnPoker.Location = new System.Drawing.Point(653, 119);
+            this.btnPoker.Name = "btnPoker";
+            this.btnPoker.Size = new System.Drawing.Size(75, 23);
+            this.btnPoker.TabIndex = 6;
+            this.btnPoker.Text = "Poker";
+            this.btnPoker.UseVisualStyleBackColor = true;
+            this.btnPoker.Click += new System.EventHandler(this.btnPoker_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(810, 549);
+            this.Controls.Add(this.btnPoker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputForm);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GamePanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.GamePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.poker5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poker1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel GamePanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InputForm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MessageBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPoker;
+        private System.Windows.Forms.PictureBox poker5;
+        private System.Windows.Forms.PictureBox poker4;
+        private System.Windows.Forms.PictureBox poker3;
+        private System.Windows.Forms.PictureBox poker2;
+        private System.Windows.Forms.PictureBox poker1;
+    }
+}
+
---
+++ PlinkoDotNet/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlinkoDotNet
+{
+    public partial class Form1 : Form
+    {
+        // Generic Folder Pathing to avoid Reusing to much code
+        const string ASSETS_FOLDER = @"C:\cerberus\Assets\cards";
+        const string PROJECT_FOLDER = @"C:\cerberus\PlinkoDotNet";
+        private static string CARD_BACK = String.Format("{0}\\{1}", ASSETS_FOLDER, "Back.png");
+        private static string CARDS_LIST = String.Format("{0}\\{1}", PROJECT_FOLDER, "Cartes.txt");
+
+        // We Load all the Cards of the file
+        // I saved already put the name of the files of my assets folder instead to save time
+        private readonly String[] cards = File.ReadAllLines(CARDS_LIST);
+
+
+        private PictureBox[] cards_images = null;
+
+        private List<String> ShuffleCards(String[] input)
+        {
+            Random r = new Random();
+            // Random Order by using empty Next, return 0 or 1 so the OrderBy will by completely Random everytime
+            return input.OrderBy(x => r.Next()).ToList();
+        }
+
+
+        private void RefreshCard(PictureBox card, String file, Boolean visible = true)
+        {
+            card.Visible = visible;
+            card.Image = Image.FromFile(file);
+            card.Refresh();
+        }
+
+        private void RefreshAllCard(Boolean visible=true)
+        {
+            foreach (var card in cards_images)
+            {
+                // Force the Back of the Card to be visible for all Cards
+                RefreshCard(card, CARD_BACK, visible);
+            }
+        }
+
+        public void Poker(string username1, int betvalue)
+        {
+            // Shuffle the Cards
+            List<String> shuffled_cards = ShuffleCards(cards);
+            Random random = new Random();
+
+            RefreshAllCard();
+            Thread.Sleep(1000);
+
+            // Pick one by one
+            foreach (var card in cards_images)
+            {
+                // let Random pick a number between 0 and our Count of shuffled Cards
+                int pick_index = random.Next(shuffled_cards.Count());
+                String picked_card = shuffled_cards[pick_index];
+
+                // Remove the picked Card to avoid Duplicate Pick
+                shuffled_cards.RemoveAt(pick_index); 
+                String file = String.Format("{0}\\{1}.png", ASSETS_FOLDER, picked_card);
+                RefreshCard(card, file, true);
+                Thread.Sleep(1000);
+            }
+
+            Thread.Sleep(2000);
+            RefreshAllCard(false);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            cards_images = new PictureBox[] { poker1, poker2, poker3, poker4, poker5 };
+        }
+
+        private void append_Text(String text)
+        {
+            MessageBox.Text += text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = InputForm.Text;
+
+            if (int.TryParse(text, out int value))
+            {
+                append_Text(String.Format("Starting a Plinko game with {0}$\n", value));
+            }
+            else
+            {
+                append_Text(String.Format("The Input is not a number {0}\n", text));
+            }
+        }
+
+        private void btnPoker_Click(object sender, EventArgs e)
+        {
+            Poker("Test", 10);
+        }
+    }
+}
